b41de63aeaa745af9cf15a229c75f0246c0c1a69
martinyounghoonkim <hoons0131@gmail.com>
2018-06-18 23:57:22 +0900

    Add example codes

---
+++ typescript/example/hello.js
@@ -0,0 +1,13 @@
+"use strict";
+var hello = 'hello';
+console.log(hello);
+var WeekDay;
+(function (WeekDay) {
+    WeekDay[WeekDay["Mon"] = 0] = "Mon";
+    WeekDay[WeekDay["Tue"] = 1] = "Tue";
+    WeekDay[WeekDay["Wed"] = 2] = "Wed";
+    WeekDay[WeekDay["Thu"] = 3] = "Thu";
+})(WeekDay || (WeekDay = {}));
+;
+var day = WeekDay.Mon;
+console.log(day);
---
+++ typescript/example/hello.ts
@@ -0,0 +1,7 @@
+const hello: string = 'hello';
+
+console.log(hello);
+
+enum WeekDay { Mon, Tue, Wed, Thu };
+let day: WeekDay = WeekDay.Mon;
+console.log(day);
